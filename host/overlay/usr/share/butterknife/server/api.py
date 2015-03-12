18847f4f22ffc548d6ff7bf652396ec4233b5cdc
Lauri Võsandi <lauri.vosandi@gmail.com>
2015-03-12 11:33:13 +0200

    Added server README

--- host/overlay/usr/share/butterknife/server/api.py
+++ host/overlay/usr/share/butterknife/server/api.py
@@ -91,6 +91,6 @@ app.add_route("/api/container/{name}/snapshot/{snap}/stream/", StreamingResource
 
 if __name__ == '__main__':
     from wsgiref import simple_server
-    httpd = simple_server.make_server('0.0.0.0', 8000, app)
+    httpd = simple_server.make_server('0.0.0.0', 80, app)
     httpd.serve_forever()
 
